e5ebb131bb292e5fb01efc77e980196799757abe
Vladimir <yovchenko.w@gmail.com>
2017-04-07 21:10:05 +0000

    Initial

--- js-jquery/index.js
+++ js-jquery/index.js
@@ -1,3 +1,11 @@
 $('h1').html('Hello J-query!');
 //('body').css('background-image', 'url("pictures/main-pg-2.png")',) ;
-$("#imageID").attr('src', 'pictures/header.png');
+//$("#imageID").attr('src', 'pictures/header.png');
+$//("#image-mobile-ID").attr('src', 'pictures/header.png');
+/*$(function() {
+  if($(window).width() <= 899) {
+    $("img #imageID").each(function() {
+      $(this).attr("src", $(this).attr("src").replace('pictures/mobile-header.png'));
+    });
+  }
+});*/
