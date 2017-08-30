504c851452e5bd2be4f618c167cffa7a607050ba
vladimir <yovchenko.w@gmail.com>
2017-08-30 11:51:08 +0300

    added form validity

--- js/form.js
+++ js/form.js
@@ -1,20 +1,34 @@
 document.querySelector('button[id=send]').addEventListener('click', sendForm);
 function sendForm(e) {
-  var form = document.querySelector('#comment');
-  if (form.checkValidity()) {
-	  e.preventDefault(); 
+  var form = $('#contactForm');
+  if (form.valid()) {
+    e.preventDefault();
     $.ajax({
-    url: "https://formspree.io/gascentr.service@gmail.com",
-    method: "POST",
-    data: {
-        textarea: $('#comment').val()
-    },
+      url: "https://formspree.io/gascentr.service@gmail.com",
+      method: "POST",
+    data: $(this).serialize(),
     dataType: "json"
 })
 .done(function() {
   $('form').html('<h1><span class="message">Спасибо, сообщение отправлено.<br> Мы свяжемся с вами в ближайшее время.</span></h1>');
 });
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 	
