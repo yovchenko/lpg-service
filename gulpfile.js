f6321725bf78dbdd0cf2959f1d2464767e50acee
yovchenko <yovchenko.w@gmail.com>
2018-01-08 18:00:48 +0200

    css minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-var gulp = require('gulp');
-var htmlmin = require('gulp-htmlmin');
-
-gulp.task('minify', function() {
-  return gulp.src('./*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
+let gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+ 
+gulp.task('minify-css', () => {
+  return gulp.src('./css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist'));
 });
