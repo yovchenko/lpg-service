0703474751c1761faf9ee50513a3ea7e3ec3e225
yovchenko <yovchenko.w@gmail.com>
2018-03-18 17:02:57 +0200

    test

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-"use strict";function myFunction(){var e=document.getElementById("menuToggle"),s=$(".breadcrumbs");if("menuButton"===e.className){e.className+=" active",s.addClass("responsive");$(".responsive").stop(!0,!0).slideDown()}else e.className="menuButton",$(".responsive").stop(!0,!0).slideUp()}var font=new FontFaceObserver("Qwigley");font.load().then(function(){document.getElementsByClassName("company-name")[0].className+=" loaded"},function(){document.getElementsByClassName("company-name")[0].className+=" error"});
+"use strict";
+
+function myFunction() {
+    var e = document.getElementById("menuToggle"),
+        s = $(".breadcrumbs");
+    if ("menuButton" === e.className) {
+        e.className += " active", s.addClass("responsive");
+        $(".responsive").stop(!0, !0).slideDown()
+    } else e.className = "menuButton", $(".responsive").stop(!0, !0).slideUp()
+}
+var font = new FontFaceObserver('Qwigley',{
+    weight: 400
+});
+font.load('GasCentr').then(function () {
+    document.getElementsByClassName("company-name")[0].className += " loaded"
+}, function () {
+    document.getElementsByClassName("company-name")[0].className += " error"
+});
