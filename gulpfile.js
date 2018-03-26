d9e4e9b2da5aec72624e40cb8c6e2c6c480daaff
yovchenko <yovchenko.w@gmail.com>
2018-03-26 22:49:40 +0300

    file minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
+var log = require('fancy-log');
 var htmlmin = require('gulp-htmlmin');
  
 gulp.task('minify', function() {
-  return gulp.src('/*.html')
+  return gulp.src('./*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .on('end', function(){ log('Done!'); });
 });
