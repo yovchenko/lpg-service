1a0ff235fd77849a52a3a381a1183cf420ac8b9a
yovchenko <yovchenko.w@gmail.com>
2018-03-29 20:23:57 +0300

    media query has been changed

--- js/index-debug.js
+++ js/index-debug.js
@@ -6,7 +6,7 @@ function myFunction() {
     if ("menuButton" === e.className) {
         e.className += " active", s.addClass("responsive");
         $(".responsive").stop(!0, !0).slideDown()
-    } else e.className = "menuButton", $(".responsive").stop(!0, !0).slideUp()
+    } else e.className = "menuButton", $(".responsive").stop(!0, !0).slideUp();
 }
 
 document.addEventListener('DOMContentLoaded',function(e){
--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
-"use strict";function myFunction(){var e=document.getElementById("menuToggle"),s=$(".breadcrumbs");"menuButton"===e.className?(e.className+=" active",s.addClass("responsive"),$(".responsive").stop(!0,!0).slideDown()):(e.className="menuButton",$(".responsive").stop(!0,!0).slideUp())}document.addEventListener("DOMContentLoaded",function(e){document.getElementsByClassName("company-name")[0].className+=" loaded"});
+"use strict";
+document.addEventListener('DOMContentLoaded',function(e){
+    document.getElementsByClassName("company-name")[0].className += " loaded";
+});
+var el = $('.menuButton'),
+    s = $('.breadcrumbs'),
+    flag = false;
+function clickFunction() {
+    if (!(el.hasClass('active'))) {
+        el.addClass("active"), 
+        s.addClass("responsive").stop(!0, !0).slideDown();
+    } else el.removeClass("active"), 
+    s.removeClass("responsive").stop(!0, !0).slideUp();
+}
+
+var mql = window.matchMedia('(min-width: 831px)');
+function screenTest(e) {
+  if (e.matches) {
+    if(s.hasClass('responsive')) flag = true;
+    else flag = false;
+    s.css("display", "block");
+  } else {
+      if(flag) {
+          s.addClass("responsive").css("display", "block");
+          flag = false;
+      }
+      else s.css("display", "none");
+   }
+}
+mql.addListener(screenTest);
