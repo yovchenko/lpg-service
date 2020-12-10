67bae1cb322dfec2768c3d8c8c3a45e3c8c6c733
yovchenko <volodymyr.yovchenko@gmail.com>
2020-12-10 23:51:44 +0200

    build(css): add postcss plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
 const gulp = require('gulp');
 const minify = require('gulp-minify');
 const htmlmin = require('gulp-htmlmin');
+const postcss = require('gulp-postcss');
+const sass = require('gulp-sass');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+gulp.task('css', function () {
+    const processors = [
+		autoprefixer,
+		cssnano
+	];
+	return gulp.src('./scss/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss(processors))
+		.pipe(gulp.dest('./css'));
+});
 
 gulp.task('compressJs', function() {
   return gulp.src('js/debug/*.js')
