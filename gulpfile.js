cc19ffc159003760ae8cd01c56a6c2848285a56d
yovchenko <volodymyr.yovchenko@gmail.com>
2021-01-08 03:07:48 +0200

    fix(index): add fancybox script

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ gulp.task('js', function() {
 
 
 gulp.task('html', () => {
-  return gulp.src('./*.html')
+  return gulp.src('./index-ua.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('dist'));
 });
